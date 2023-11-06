0800d4214875c46ce609ae66fc55083a35068aec
teddav <teddav@users.noreply.github.com>
2023-11-06 11:04:44 +0100

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,3 @@ pub fn read_file(day: &str) -> io::Result<String> {
     let contents = fs::read_to_string(path)?;
     Ok(contents)
 }
-
-pub fn read_and_parse_file(day: &str) -> io::Result<Vec<String>> {
-    let path = Path::new("inputs").join(format!("day{day}.txt"));
-    let contents = fs::read_to_string(path)?;
-    let splitted: Vec<String> = contents
-        .as_str()
-        .trim()
-        .split('\n')
-        .map(|s| s.to_string())
-        .collect();
-    Ok(splitted)
-}
-
-pub fn print_type_of<T>(_: &T) {
-    println!(" type: {}", std::any::type_name::<T>());
-}
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-#[path = "bin/day01.rs"]
-mod day01;
-
-fn main() {
-    println!("-----> RUNNING MAIN !");
-    day01::main();
-}
